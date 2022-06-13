cc49970b691240f6ee052116e2004bb76f207841
Övünç Yayla <ovuncyayla@gmail.com>
2022-06-13 22:31:24 +0200

    day 02 part 2

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,36 @@ struct Command(String, i32);
 #[derive(Debug)]
 struct Position {
     x: i32,
-    y: i32,
+    y: i32, // aim
 }
 
 struct Submarine {
     position: Position,
+    depth: i32
 }
 
 impl Submarine {
     pub fn new() -> Submarine {
         Submarine {
             position: Position { x: 0, y: 0 },
+            depth: 0
         }
     }
 
     pub fn process_cmd(&mut self, cmd: Command) {
         match cmd.0.as_str() {
-            "forward" => self.position.x += cmd.1,
+            "forward" => {
+                self.position.x += cmd.1;
+                self.depth += self.position.y * cmd.1
+            }
             "down" => self.position.y += cmd.1,
             "up" => self.position.y -= cmd.1,
             _ => unreachable!(),
         }
     }
 
-    pub fn multiplied_pos(&self) -> i32 {
-        self.position.x * self.position.y
+    pub fn multiplied_depth(&self) -> i32 {
+        self.position.x * self.depth
     }
 }
 
@@ -43,9 +48,10 @@ fn main() {
     let mut submarine = Submarine::new();
     for cmd in inp {
         submarine.process_cmd(cmd);
-        //println!("{:#?}", cmd);
     }
 
     println!("Final pos: {:?}", submarine.position);
-    println!("Final pos multiplied: {:?}", submarine.multiplied_pos());
+    println!("Final depth: {:?}", submarine.depth);
+    println!("Final pos multiplied: {:?}", submarine.multiplied_depth());
 }
+
