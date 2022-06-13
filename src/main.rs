43930c520e675447a8c56b5ee0464a9673548e3a
Övünç Yayla <ovuncyayla@gmail.com>
2022-06-13 21:46:41 +0200

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,51 @@
-// Advent of Code - Day 1
+// Advent of Code - Day 2
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Command(String, i32);
+
+#[derive(Debug)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+struct Submarine {
+    position: Position,
+}
+
+impl Submarine {
+    pub fn new() -> Submarine {
+        Submarine {
+            position: Position { x: 0, y: 0 },
+        }
+    }
+
+    pub fn process_cmd(&mut self, cmd: Command) {
+        match cmd.0.as_str() {
+            "forward" => self.position.x += cmd.1,
+            "down" => self.position.y += cmd.1,
+            "up" => self.position.y -= cmd.1,
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn multiplied_pos(&self) -> i32 {
+        self.position.x * self.position.y
+    }
+}
+
 fn main() {
     // Read input
     let input = std::fs::read_to_string("./input.ron").unwrap();
-    let inp: Vec<i32> = ron::from_str(input.as_str()).unwrap();
+    let inp: Vec<Command> = ron::from_str(input.as_str()).unwrap();
 
-    // Count measurements
-    let cnt: i32 =
-        inp.iter()
-            .enumerate()
-            .skip(1)
-            .map(|(i, val)| {
-                if inp[i - 1] < *val { 1 } else { 0 }
-            }).sum();
+    let mut submarine = Submarine::new();
+    for cmd in inp {
+        submarine.process_cmd(cmd);
+        //println!("{:#?}", cmd);
+    }
 
-    println!("Total measurements larger than previous one: {}", cnt);
+    println!("Final pos: {:?}", submarine.position);
+    println!("Final pos multiplied: {:?}", submarine.multiplied_pos());
 }
