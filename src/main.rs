0dbc0d673843c5041685248110e8d92419a0c583
Övünç Yayla <ovuncyayla@gmail.com>
2022-06-15 16:53:36 +0200

    day_03

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,71 @@
-// Advent of Code - Day 2
-use serde::Deserialize;
+// Advent of Code - Day 3
+use std::{
+    collections::BTreeMap,
+    io::{BufRead, BufReader},
+};
 
-#[derive(Deserialize, Debug)]
-struct Command(String, i32);
-
-#[derive(Debug)]
-struct Position {
-    x: i32,
-    y: i32,
+struct DiagnosticReport {
+    digits: BTreeMap<i32, Vec<i32>>,
 }
 
-struct Submarine {
-    position: Position,
-}
-
-impl Submarine {
-    pub fn new() -> Submarine {
-        Submarine {
-            position: Position { x: 0, y: 0 },
+impl DiagnosticReport {
+    pub fn new() -> DiagnosticReport {
+        DiagnosticReport {
+            digits: BTreeMap::new(),
         }
     }
 
-    pub fn process_cmd(&mut self, cmd: Command) {
-        match cmd.0.as_str() {
-            "forward" => self.position.x += cmd.1,
-            "down" => self.position.y += cmd.1,
-            "up" => self.position.y -= cmd.1,
-            _ => unreachable!(),
-        }
+    pub fn add_digit(&mut self, index: i32, digit: i32) {
+        self.digits.entry(index).or_insert(vec![]);
+        self.digits.get_mut(&index).unwrap().push(digit);
     }
 
-    pub fn multiplied_pos(&self) -> i32 {
-        self.position.x * self.position.y
+    pub fn calc_energy_consumption(&self) {
+        let mut rate_digits = String::new();
+
+        for val in self.digits.values() {
+            let mut cnt_0 = 0;
+            let mut cnt_1 = 1;
+            val.iter()
+                .map(|x| {
+                    if *x == 0 {
+                        cnt_0 += 1;
+                    } else {
+                        cnt_1 += 1;
+                    }
+                })
+                .count();
+            let digit = if cnt_0 > cnt_1 { 0 } else { 1 };
+            rate_digits.push_str(digit.to_string().as_str());
+        }
+
+        println!("Rate Digits: {}", rate_digits);
+        let gama_rate = i32::from_str_radix(&rate_digits, 2).unwrap();
+        let epsilon_rate = !(i32::MAX << rate_digits.len()) & (!gama_rate);
+        println!("Gama Rate: {} - {:b}", gama_rate, gama_rate);
+        println!("Epsilon Rate: {} - {:b}", epsilon_rate, epsilon_rate);
+        println!("Power Consumption: {}", gama_rate * epsilon_rate);
     }
 }
 
 fn main() {
     // Read input
-    let input = std::fs::read_to_string("./input.ron").unwrap();
-    let inp: Vec<Command> = ron::from_str(input.as_str()).unwrap();
+    let file = std::fs::File::open("./input.ron").unwrap();
+    let reader = BufReader::new(file);
 
-    let mut submarine = Submarine::new();
-    for cmd in inp {
-        submarine.process_cmd(cmd);
-        //println!("{:#?}", cmd);
+    let mut report = DiagnosticReport::new();
+    for l in reader.lines() {
+        let line = l.as_ref().unwrap();
+        let chars = line.chars();
+        for (index, c) in chars.enumerate() {
+            let i = index as i32;
+            match c {
+                '0' => report.add_digit(i, 0),
+                '1' => report.add_digit(i, 1),
+                _ => unreachable!(),
+            }
+        }
     }
 
-    println!("Final pos: {:?}", submarine.position);
-    println!("Final pos multiplied: {:?}", submarine.multiplied_pos());
+    report.calc_energy_consumption();
 }
